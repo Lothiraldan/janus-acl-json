4b6ae99a7476cad25789e0d30c934948df806cf8
FELD Boris <lothiraldan@gmail.com>
2012-11-27 17:13:27 +0100

    Import request protocol demo and rename it to propose protocol demo

---
+++ janus-propose-demo/src/main/java/org/janusproject/demos/acl/request/Launcher.java
@@ -0,0 +1,65 @@
+/* 
+ * $Id$
+ * 
+ * Janus platform is an open-source multiagent platform.
+ * More details on <http://www.janus-project.org>
+ * Copyright (C) 2012 Janus Core Developers
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.janusproject.demos.acl.request;
+
+import java.util.logging.Level;
+
+import org.arakhne.vmutil.locale.Locale;
+import org.janusproject.demos.acl.request.agent.ACLProtocolReceiver;
+import org.janusproject.demos.acl.request.agent.ACLProtocolSender;
+import org.janusproject.kernel.Kernel;
+import org.janusproject.kernel.agent.Kernels;
+import org.janusproject.kernel.logger.LoggerUtil;
+
+/**
+ * DEMO : Request Protocol
+ * 
+ * @author $Author: madeline$
+ * @author $Author: kleroy$
+ * @author $Author: ptalagrand$
+ * @author $Author: ngaud$
+ * @version $FullVersion$
+ * @mavengroupid $Groupid$
+ * @mavenartifactid $ArtifactId$
+ */
+public class Launcher {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		LoggerUtil.setGlobalLevel(Level.ALL);
+		LoggerUtil.setShortLogMessageEnable(true);
+		
+		Kernel k = Kernels.get( false );
+		
+		ACLProtocolSender sender = new ACLProtocolSender();
+		ACLProtocolReceiver receiver = new ACLProtocolReceiver();
+		
+		k.submitLightAgent(sender, Locale.getString(Launcher.class, "SENDER")); //$NON-NLS-1$
+		k.submitLightAgent(receiver, Locale.getString(Launcher.class, "RECEIVER")); //$NON-NLS-1$
+		
+		k.launchDifferedExecutionAgents();	
+		
+		Kernels.killAll();
+	}	
+}
