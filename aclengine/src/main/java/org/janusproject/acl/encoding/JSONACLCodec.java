914ffa644480f3575c8b8d8158402c542b4218b8
FELD Boris <lothiraldan@gmail.com>
2012-11-27 15:48:58 +0100

    Move from JSON to Jackson

--- aclengine/src/main/java/org/janusproject/acl/encoding/JSONACLCodec.java
+++ aclengine/src/main/java/org/janusproject/acl/encoding/JSONACLCodec.java
@@ -1,9 +1,12 @@
 package org.janusproject.acl.encoding;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -11,75 +14,74 @@ import org.janusproject.acl.ACLMessage;
 import org.janusproject.acl.ACLMessageContent;
 import org.janusproject.kernel.address.AgentAddress;
 import org.janusproject.kernel.crio.core.AddressUtil;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import de.undercouch.bson4jackson.BsonFactory;
 
 public class JSONACLCodec implements ACLMessageContentEncodingService {
 
 	@Override
 	public byte[] encode(ACLMessage aMsg) {
-		JSONObject output = new JSONObject();
-		try {
-			// Performative
-			output.put("performative", aMsg.getPerformative().ordinal());
 
-			// Display SENDER
-			AgentAddress sender = aMsg.getSender();
-			if (sender != null) {
-				Map<String, String> sender_infos = new HashMap<String, String>();
-				sender_infos.put("name", sender.getName());
-				sender_infos.put("id", sender.getUUID().toString());
-				output.put("sender", sender_infos);
-			}
+		Map<String, Object> output = new HashMap<String, Object>();
 
-			// Display RECEIVERS
-			Collection<AgentAddress> receivers = aMsg.getReceiver();
-			Collection<Map<String, String>> receivers_infos = new ArrayList<Map<String, String>>();
+		// Performative
+		output.put("performative", aMsg.getPerformative().ordinal());
 
-			if (receivers != null) {
-				for (AgentAddress receiver : receivers) {
-					Map<String, String> receiver_info = new HashMap<String, String>();
-					receiver_info.put("name", receiver.getName());
-					receiver_info.put("id", receiver.getUUID().toString());
-					receivers_infos.add(receiver_info);
-				}
-				output.put("receiver", receivers_infos);
-			}
-
-			// Display CONTENT
-			String content = aMsg.getContent().getContent().toString();
-			if (content != null && content.length() > 0) {
-				output.put("content", content.trim());
-			}
-
-			// Display ENCODING
-			output.put("encoding", aMsg.getEncoding());
-
-			// Display LANGUAGE
-			output.put("language", aMsg.getLanguage());
-
-			// Display ONTOLOGY
-			output.put("ontology", aMsg.getOntology());
-
-			// Display PROTOCOL
-			output.put("protocol", aMsg.getProtocol().getName());
-
-			// Display CONVERSATION ID
-			output.put("conversationid", aMsg.getConversationId());
-
-		} catch (JSONException e) {
-			e.printStackTrace();
-			return null;
+		// Display SENDER
+		AgentAddress sender = aMsg.getSender();
+		if (sender != null) {
+			Map<String, String> sender_infos = new HashMap<String, String>();
+			sender_infos.put("name", sender.getName());
+			sender_infos.put("id", sender.getUUID().toString());
+			output.put("sender", sender_infos);
 		}
 
-		System.out.println(output.toString());
-		return output.toString().getBytes();
+		// Display RECEIVERS
+		Collection<AgentAddress> receivers = aMsg.getReceiver();
+		Collection<Map<String, String>> receivers_infos = new ArrayList<Map<String, String>>();
+
+		if (receivers != null) {
+			for (AgentAddress receiver : receivers) {
+				Map<String, String> receiver_info = new HashMap<String, String>();
+				receiver_info.put("name", receiver.getName());
+				receiver_info.put("id", receiver.getUUID().toString());
+				receivers_infos.add(receiver_info);
+			}
+			output.put("receiver", receivers_infos);
+		}
+
+		// Display CONTENT
+		String content = aMsg.getContent().getContent().toString();
+		if (content != null && content.length() > 0) {
+			output.put("content", content.trim());
+		}
+
+		// Display ENCODING
+		output.put("encoding", aMsg.getEncoding());
+
+		// Display LANGUAGE
+		output.put("language", aMsg.getLanguage());
+
+		// Display ONTOLOGY
+		output.put("ontology", aMsg.getOntology());
+
+		// Display PROTOCOL
+		output.put("protocol", aMsg.getProtocol().getName());
+
+		// Display CONVERSATION ID
+		output.put("conversationid", aMsg.getConversationId());
+
+		return fromMap(output);
 	}
 
 	@Override
 	public ACLMessageContent decode(byte[] byteMsg, Object... parameters) {
-        System.out.println(new String(byteMsg));
+		System.out.println(new String(byteMsg));
 		ACLMessage.Content content = new ACLMessage.Content();
 
 		// Get charset parameter
@@ -99,71 +101,104 @@ public class JSONACLCodec implements ACLMessageContentEncodingService {
 			return null;
 		}
 
-		JSONObject json = null;
-		try {
-			json = new JSONObject(message);
+		Map<String, Object> json = fromBytes(message);
+		;
 
-			// PERFORMATIVE
-			content.setPerformative(json.getInt("performative"));
+		// PERFORMATIVE
+		content.setPerformative((Integer) json.get("performative"));
 
-			// SENDER
-			if (json.has("sender")) {
-				String sender_id = json.getJSONObject("sender").getString("id");
-				content.setSender(AddressUtil.createAgentAddress(UUID
-						.fromString(sender_id)));
+		// SENDER
+		if (json.containsKey("sender")) {
+			String sender_id = (String) ((Map<String, Object>) json
+					.get("sender")).get("id");
+			content.setSender(AddressUtil.createAgentAddress(UUID
+					.fromString(sender_id)));
+		}
+
+		// RECEIVERS
+		if (json.containsKey("receiver")) {
+			ArrayList<Map<String, Object>> json_receivers = (ArrayList<Map<String, Object>>) json
+					.get("receiver");
+			Collection<AgentAddress> receivers = new ArrayList<AgentAddress>();
+
+			for (int i = 0; i < json_receivers.size(); i++) {
+				String receiver_info = (String) json_receivers.get(i).get("id");
+				receivers.add(AddressUtil.createAgentAddress(UUID
+						.fromString(receiver_info)));
 			}
+			content.setReceiver(receivers);
+		}
 
-			// RECEIVERS
-			if (json.has("receiver")) {
-				JSONArray json_receivers = json.getJSONArray("receiver");
-				Collection<AgentAddress> receivers = new ArrayList<AgentAddress>();
+		// CONTENT
+		if (json.containsKey("content")) {
+			content.setContent(new StringBuffer((String) json.get("content")));
+		}
 
-				for (int i = 0; i < json_receivers.length(); i++) {
-					String receiver_info = json_receivers.getJSONObject(i)
-							.getString("id");
-					receivers.add(AddressUtil.createAgentAddress(UUID
-							.fromString(receiver_info)));
-				}
-				content.setReceiver(receivers);
+		// ENCODING
+		content.setEncoding((String) json.get("encoding"));
+
+		// LANGUAGE
+		if (json.containsKey("language")) {
+			content.setLanguage((String) json.get("language"));
+		}
+
+		// ONTOLOGY
+		if (json.containsKey("ontology")) {
+			content.setOntology((String) json.get("ontology"));
+		}
+
+		// PROTOCOL
+		content.setProtocol((String) json.get("protocol"));
+
+		// CONVERSATION ID
+		if (json.containsKey("conversationid")
+				&& json.get("conversationid") != null) {
+
+			try {
+				String uuid = (String) json.get("conversationid");
+				content.setConversationId(UUID.fromString(uuid));
+			} catch (IllegalArgumentException e) {
+				content.setConversationId(null);
 			}
-
-			// CONTENT
-			if (json.has("content")) {
-				content.setContent(new StringBuffer(json.getString("content")));
-			}
-
-			// ENCODING
-			content.setEncoding(json.getString("encoding"));
-
-			// LANGUAGE
-			if (json.has("language")) {
-				content.setLanguage(json.getString("language"));
-			}
-
-			// ONTOLOGY
-			if (json.has("ontology")) {
-				content.setOntology(json.getString("ontology"));
-			}
-
-			// PROTOCOL
-			content.setProtocol(json.getString("protocol"));
-
-			// CONVERSATION ID
-			if (json.has("conversationid")) {
-				String uuid = json.getString("conversationid");
-
-				try {
-					content.setConversationId(UUID.fromString(uuid));
-				} catch (IllegalArgumentException e) {
-					content.setConversationId(null);
-				}
-			}
-
-		} catch (JSONException e) {
-			e.printStackTrace();
-			return null;
 		}
 
 		return content;
+	}
+
+	private byte[] fromMap(Map m) {
+		ObjectMapper mapper = new ObjectMapper();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			mapper.writeValue(baos, m);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return baos.toByteArray();
+	}
+
+	private Map<String, Object> fromBytes(String src) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(src,
+					new TypeReference<Map<String, Object>>() {
+					});
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
