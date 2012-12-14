eb3eb46e153fb03dd18ae564585efc8382827ca8
FELD Boris <lothiraldan@gmail.com>
2012-12-14 23:29:47 +0100

    Work on gui

---
+++ janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/gui/AgentFrame.java
@@ -0,0 +1,13 @@
+package org.janusproject.demos.meetingscheduler.gui;
+
+import org.janusproject.demos.meetingscheduler.agent.BaseAgent;
+
+public class AgentFrame {
+
+	private BaseAgent agent;
+
+	public AgentFrame(BaseAgent agent) {
+		this.agent = agent;
+	}
+
+}
