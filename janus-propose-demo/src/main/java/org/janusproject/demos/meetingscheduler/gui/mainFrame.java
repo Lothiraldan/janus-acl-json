6d1060dd43bf341aa59ea00efd4b3bbab155dcf7
Nicolas Grenie <nicolas.grenie@gmail.com>
2012-12-14 22:37:44 +0100

    mainFrame GUI + prompt to add new user

--- janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/gui/mainFrame.java
+++ janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/gui/mainFrame.java
@@ -3,17 +3,21 @@ package org.janusproject.demos.meetingscheduler.gui;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JWindow;
 
-public class mainFrame extends JFrame{
+public class mainFrame extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 3830079646720453065L;
 
@@ -25,6 +29,9 @@ public class mainFrame extends JFrame{
 		
 		JLabel welcomeLabel = new JLabel("Welcome to Janus MeetingScheduler demo");
 		JLabel instructionLabel = new JLabel("Add participants to start");
+		JButton addParticipantButton =  new JButton("Add Participant");
+		addParticipantButton.setActionCommand("ADDPARTICIPANT");
+		addParticipantButton.addActionListener(this);
 		
 		String[] columnNames = {"Participant name"};
 
@@ -37,6 +44,7 @@ public class mainFrame extends JFrame{
 		};
 		
 		final JTable table = new JTable(data, columnNames);
+		table.setSize(400,200);
 	
 		table.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
@@ -50,7 +58,24 @@ public class mainFrame extends JFrame{
 		//this.pack();
 		this.add(welcomeLabel);
 		this.add(instructionLabel);
+		this.add(addParticipantButton);
 		this.add(scrollPane);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		String cmd = evt.getActionCommand();
+		System.out.println("ahaha");
+		if (cmd == "ADDPARTICIPANT") {
+		     System.out.println(addParticipantPrompt());
+		} else if (cmd == "CANCEL") {
+		     System.out.println("jjiji");
+		}
+	}
+	
+	public String addParticipantPrompt(){
+		JFrame frame = new JFrame("InputDialog Example #1");
+		String name = JOptionPane.showInputDialog(frame, "What's your name?");
+		return name;
+	}
 }
