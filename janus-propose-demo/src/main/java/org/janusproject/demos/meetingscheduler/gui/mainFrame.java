2f5687ccb8386e7548e0762bc2e9226d65aab115
Nicolas Grenie <nicolas.grenie@gmail.com>
2012-12-15 01:22:11 +0100

    event listener on mainframe list

--- janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/gui/mainFrame.java
+++ janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/gui/mainFrame.java
@@ -18,47 +18,52 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JWindow;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-public class mainFrame extends JFrame implements ActionListener{
+public class mainFrame extends JFrame implements ActionListener,
+		ListSelectionListener {
 
 	private static final long serialVersionUID = 3830079646720453065L;
 	private ActionListener listener;
 	private MeetingSchedulor ms;
-	
+
 	private JList agentNameList;
 
-	public mainFrame(){
+	public mainFrame() {
 		Container contentPane = this.getContentPane();
-		contentPane.setLayout(new BoxLayout(contentPane, getDefaultCloseOperation()));
-		this.setSize(400,500);
-		this.setLocation(100,100);
-		
-		JLabel welcomeLabel = new JLabel("Welcome to Janus MeetingScheduler demo");
+		contentPane.setLayout(new BoxLayout(contentPane,
+				getDefaultCloseOperation()));
+		this.setSize(400, 500);
+		this.setLocation(100, 100);
+
+		JLabel welcomeLabel = new JLabel(
+				"Welcome to Janus MeetingScheduler demo");
 		JLabel instructionLabel = new JLabel("Add participants to start");
-		JButton addParticipantButton =  new JButton("Add Participant");
+		JButton addParticipantButton = new JButton("Add Participant");
 		addParticipantButton.setActionCommand("ADDPARTICIPANT");
 		addParticipantButton.addActionListener(this);
-		
+
 		agentNameList = new JList();
-		agentNameList.setSize(400,200);
-	
+		agentNameList.setSize(400, 200);
+		agentNameList.addListSelectionListener(this);
+
 		agentNameList.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-			    System.out.println(agentNameList);
+				System.out.println(agentNameList);
 			}
 		});
-		
+
 		JScrollPane scrollPane = new JScrollPane(agentNameList);
 
-		
-		//this.pack();
+		// this.pack();
 		this.add(welcomeLabel);
 		this.add(instructionLabel);
 		this.add(addParticipantButton);
 		this.add(scrollPane);
 	}
-	
-	public void setSchedulor(MeetingSchedulor ms){
+
+	public void setSchedulor(MeetingSchedulor ms) {
 		this.ms = ms;
 	}
 
@@ -66,26 +71,37 @@ public class mainFrame extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent evt) {
 		String cmd = evt.getActionCommand();
 		if (cmd == "ADDPARTICIPANT") {
-		     this.ms.addAgent(addParticipantPrompt());
-		     updateList();
-		     
+			this.ms.addAgent(addParticipantPrompt());
+			updateList();
+
 		} else if (cmd == "CANCEL") {
-		     System.out.println("jjiji");
+			System.out.println("jjiji");
 		}
 	}
-	
-	public String addParticipantPrompt(){
+
+	public String addParticipantPrompt() {
 		JFrame frame = new JFrame("InputDialog Example #1");
 		String name = JOptionPane.showInputDialog(frame, "What's your name?");
 		return name;
 	}
-	
-	public void updateList(){
+
+	public void updateList() {
 		agentNameList.removeAll();
 		DefaultListModel dlm = new DefaultListModel();
-		for(String elem:this.ms.getAllAgents()){
+		for (String elem : this.ms.getAllAgents()) {
 			dlm.addElement(elem);
 		}
 		agentNameList.setModel(dlm);
 	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		JList list = (JList) e.getSource();
+		int selections[] = list.getSelectedIndices();
+		Object selectionValues[] = list.getSelectedValues();
+		for (int i = 0, n = selections.length; i < n; i++) {
+			this.ms.showAgentFrame((String)selectionValues[i]);
+		}
+	}
+
 }
