3c3a3cf36e8b448c1c1071289089dc13ee30de55
FELD Boris <lothiraldan@gmail.com>
2012-12-20 13:45:31 +0100

    Add a description field to meeting

--- janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/ontology/Meeting.java
+++ janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/ontology/Meeting.java
@@ -4,9 +4,11 @@ import java.util.Date;
 
 public class Meeting {
 	private Calendar calendar;
+	private String description;
 
-	public Meeting(Calendar calendar) {
+	public Meeting(Calendar calendar, String description) {
 		super();
 		this.calendar = calendar;
+		this.description = description;
 	}
 }
