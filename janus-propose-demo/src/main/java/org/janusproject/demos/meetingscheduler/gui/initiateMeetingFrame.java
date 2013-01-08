aceda7974803b2c843c66991283372cc944852a0
FELD Boris <lothiraldan@gmail.com>
2013-01-08 17:01:52 +0100

    Fix initiateMeetingFrame

--- janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/gui/initiateMeetingFrame.java
+++ janus-propose-demo/src/main/java/org/janusproject/demos/meetingscheduler/gui/initiateMeetingFrame.java
@@ -1,14 +1,12 @@
 package org.janusproject.demos.meetingscheduler.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -23,10 +21,12 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
-import javax.swing.ListModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.janusproject.demos.meetingscheduler.ontology.Meeting;
 import org.janusproject.demos.meetingscheduler.util.KernelWatcher;
@@ -39,7 +39,6 @@ import com.miginfocom.theme.Themes;
 import com.miginfocom.util.dates.DateChangeEvent;
 import com.miginfocom.util.dates.DateChangeListener;
 import com.miginfocom.util.dates.DateRange;
-import com.miginfocom.util.dates.DateRangeI;
 import com.miginfocom.util.dates.ImmutableDateRange;
 
 public class initiateMeetingFrame extends JFrame implements ActionListener,
@@ -48,13 +47,12 @@ public class initiateMeetingFrame extends JFrame implements ActionListener,
 	private KernelWatcher kw;
 
 	private JList<String> participantList;
-	private JList<ImmutableDateRange> hoursList;
-	
-	private JList<String> suggestedHoursList;
-	private JList<String> selectedHoursList;
-	private DefaultListModel<String> suggestedHoursListModel = new DefaultListModel<String>();;
-	private DefaultListModel<String> selectedHoursListModel = new DefaultListModel<String>();;
-	
+
+	private JList<ImmutableDateRange> suggestedHoursList;
+	private JList<ImmutableDateRange> selectedHoursList;
+	private DefaultListModel<ImmutableDateRange> suggestedHoursListModel = new DefaultListModel<ImmutableDateRange>();
+	private DefaultListModel<ImmutableDateRange> selectedHoursListModel = new DefaultListModel<ImmutableDateRange>();
+
 	private String initiator_name;
 	private JTextField description_field;
 
@@ -62,6 +60,10 @@ public class initiateMeetingFrame extends JFrame implements ActionListener,
 	private DatePicker datePicker;
 
 	private ActivityDepository depository;
+
+	private ImmutableDateRange range;
+
+	private JSpinner hoursSpinner;
 
 	private static final long serialVersionUID = 234360639496126275L;
 
@@ -87,49 +89,48 @@ public class initiateMeetingFrame extends JFrame implements ActionListener,
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		
+
 		JPanel listbox = new JPanel();
 		listbox.setLayout(new BoxLayout(listbox, BoxLayout.LINE_AXIS));
-		
-		suggestedHoursList = new JList<String>(suggestedHoursListModel);
-		selectedHoursList= new JList<String>(selectedHoursListModel);
-		
+
+		suggestedHoursList = new JList<ImmutableDateRange>(
+				suggestedHoursListModel);
+		selectedHoursList = new JList<ImmutableDateRange>(
+				selectedHoursListModel);
+
 		JPanel list_left = new JPanel();
 		list_left.setLayout(new BoxLayout(list_left, BoxLayout.Y_AXIS));
 		list_left.add(new JLabel("Suggested timeslots"));
-		list_left.add(suggestedHoursList);
-		
+		list_left.add(new JScrollPane(suggestedHoursList));
+
 		JPanel list_right = new JPanel();
 		list_right.setLayout(new BoxLayout(list_right, BoxLayout.Y_AXIS));
 		list_right.add(new JLabel("Selected timeslots"));
-		list_right.add(selectedHoursList);
-		
-		
+		list_right.add(new JScrollPane(selectedHoursList));
+
 		listbox.add(list_left);
-		
+
 		JButton toRight = new JButton("<<");
 		listbox.add(toRight);
 		toRight.setActionCommand("ADDLEFT");
 		toRight.addActionListener(this);
-		
+
 		JButton toLeft = new JButton(">>");
 		listbox.add(toLeft);
 		toLeft.setActionCommand("ADDRIGHT");
 		toLeft.addActionListener(this);
-		
+
 		listbox.add(list_right);
-				
+
 		description_field = new JTextField("Description");
 
-		participantList = new JList(this.kw.getAllAgentExcept(name).toArray(new String[0]));
+		participantList = new JList<String>(this.kw.getAllAgentExcept(name)
+				.toArray(new String[0]));
 		JScrollPane scrollPaneParticipants = new JScrollPane(participantList);
 
-		hoursList = new JList<ImmutableDateRange>();
-		JScrollPane scrollPaneHours = new JScrollPane(hoursList);
-		
 		JPanel bottombox = new JPanel();
 		bottombox.setLayout(new BoxLayout(bottombox, BoxLayout.Y_AXIS));
-		
+
 		JButton sendProposalButton = new JButton("Send meeting proposal");
 		sendProposalButton.setActionCommand("SENDMEETING");
 		sendProposalButton.addActionListener(this);
@@ -139,7 +140,7 @@ public class initiateMeetingFrame extends JFrame implements ActionListener,
 
 		panel.add(createDatePickerPanel(), BorderLayout.NORTH);
 		panel.add(scrollPaneParticipants, BorderLayout.EAST);
-		panel.add(listbox,BorderLayout.CENTER);
+		panel.add(listbox, BorderLayout.CENTER);
 		panel.add(bottombox, BorderLayout.SOUTH);
 		this.add(panel);
 	}
@@ -158,76 +159,67 @@ public class initiateMeetingFrame extends JFrame implements ActionListener,
 		panel.add(new JLabel("Day Select:"));
 		panel.add(datePicker);
 		panel.add(new JLabel("Number of hours of the meeting"));
-		JSpinner hoursSpinner = new JSpinner();
+		this.hoursSpinner = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1));
+		this.hoursSpinner.addChangeListener(new ChangeListener() {
+			
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				refreshSlotList();
+			}
+		});
 		panel.add(hoursSpinner);
-		JButton getRangeButton = new JButton("Get ranges");
-		panel.add(getRangeButton);
-		
-		getRangeButton.addActionListener(new ActionListener() {
-			 
-            public void actionPerformed(ActionEvent e)
-            {
-            	refreshSlotList();	
-                System.out.println("You clicked the button");
-            }
-        });
-
 		panel.setOpaque(false);
 
 		return panel;
 	}
-	
-	public List<ImmutableDateRange> generateDateRange(DateRangeI s, int nbHours){
-		ImmutableDateRange start = new ImmutableDateRange(s);
-		//for(ImmutableDateRange i = start; i.getStartTime().getTime())
-		return null;
-		
-	}
-	
-	public void refreshSlotList(){
-		Date today = new Date();
-		System.out.println(today.toString()+ " "+today.getDate());
-		int range = 2;
-		long hour = 3600 * 1000; 
-		List<String> slots = new ArrayList();
-		int max = today.getDate()+1;
-		Date d = today;
-		while(d.getDate()<max){
-			Date addrange = new Date ();
-			addrange.setTime(d.getTime());
-			addrange.setTime(addrange.getTime()+range*hour);
-			slots.add(""+d.getDate()+" "+d.getHours()+":"+d.getMinutes()+ " to "+addrange.getDate()+" "+addrange.getHours()+":"+addrange.getMinutes());
-			d.setTime(d.getTime()+range*hour);
+
+	public void refreshSlotList() {
+		if (this.range == null) {
+			return;
 		}
-		suggestedHoursList.removeAll();
-		
-		for (String s : slots) {
-		   suggestedHoursListModel.addElement( s);
-	   }
-		
+
+		ActivityList existingActivites = depository.getActivities();
+
+		Integer rangeHours = (Integer) this.hoursSpinner.getValue();
+
+		suggestedHoursListModel.removeAllElements();
+		@SuppressWarnings("unchecked")
+		Iterator<ImmutableDateRange> x = this.range.iterator(
+				DateRange.RANGE_TYPE_HOUR, rangeHours);
+		while (x.hasNext()) {
+			ImmutableDateRange date = x.next();
+			if (!existingActivites
+					.hasOverlapping(date.getDateRangeForReading())) {
+				suggestedHoursListModel.addElement(date);
+			}
+		}
 	}
-	
 
 	@Override
 	public void actionPerformed(ActionEvent evt) {
 		String cmd = evt.getActionCommand();
 		if (cmd == "SENDMEETING") {
 			List<String> participants = participantList.getSelectedValuesList();
-			List<ImmutableDateRange> hours = hoursList.getSelectedValuesList();
+			List<ImmutableDateRange> hours = Collections
+					.list(selectedHoursListModel.elements());
 			String description = description_field.getText();
 			Meeting meeting = new Meeting(this.initiator_name, hours,
 					description);
 			this.kw.getChannel(this.initiator_name).createMeeting(meeting,
 					this.kw.getAgentByNames(participants));
 			this.dispose();
-		}else if(cmd=="ADDRIGHT"){ // >>
-			selectedHoursListModel.addElement(suggestedHoursList.getSelectedValue());
-			suggestedHoursListModel.removeElement(suggestedHoursList.getSelectedValue());
-			suggestedHoursList.setSelectedIndex(0);
-		}else if(cmd=="ADDLEFT"){ // <<
-			suggestedHoursListModel.addElement(selectedHoursList.getSelectedValue());
-			selectedHoursListModel.removeElement(selectedHoursList.getSelectedValue());
-			selectedHoursList.setSelectedIndex(0);
+		} else if (cmd == "ADDRIGHT") { // >>
+			for (ImmutableDateRange range : suggestedHoursList
+					.getSelectedValuesList()) {
+				selectedHoursListModel.addElement(range);
+				suggestedHoursListModel.removeElement(range);
+			}
+		} else if (cmd == "ADDLEFT") { // <<
+			for (ImmutableDateRange range : selectedHoursList
+					.getSelectedValuesList()) {
+				suggestedHoursListModel.addElement(range);
+				selectedHoursListModel.removeElement(range);
+			}
 		}
 	}
 
@@ -245,22 +237,9 @@ public class initiateMeetingFrame extends JFrame implements ActionListener,
 			return;
 		}
 
-		DefaultListModel<ImmutableDateRange> data = new DefaultListModel<ImmutableDateRange>();
+		this.range = range;
 
-		ActivityList existingActivites = depository.getActivities();
-
-		@SuppressWarnings("unchecked")
-		Iterator<ImmutableDateRange> x = range.iterator(
-				DateRange.RANGE_TYPE_HOUR, 2);
-		while (x.hasNext()) {
-			ImmutableDateRange date = x.next();
-			if (!existingActivites
-					.hasOverlapping(date.getDateRangeForReading())) {
-				data.addElement(date);
-			}
-		}
-		hoursList.removeAll();
-		hoursList.setModel(data);
+		this.refreshSlotList();
 	}
 
 }
